e1f1ba8c380edb3155c661dd503c48f59f559b44
Maminirina Edwino <edwinomainirina@gmail.com>
2026-02-23 17:33:40 +0300

    implement: formation model

--- src/models/formation.php
+++ src/models/formation.php
@@ -1,4 +1,22 @@
 <?php
 class Formation{
-    
+    private int $id_formation; 
+    private string $nom_formation;
+    private DateTime $created_at;
+
+    public function getId_formation(): int {
+        return $this->id_formation;
+    }
+
+    public function getNom_formation(): string {
+        return $this->nom_formation;
+    }
+
+    public function setNom_formation(string $nom_formation){
+        $this->nom_formation = $nom_formation;
+    }
+
+    public function getCreated_at(): DateTime{
+        return $this->created_at;
+    }
 }
