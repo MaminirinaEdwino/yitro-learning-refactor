c15420af0f9c3dab204703d25cb9caf0349ec40b
Maminirina Edwino <edwinomainirina@gmail.com>
2026-02-23 17:42:52 +0300

    implement: cours model

---
+++ src/models/cours.php
@@ -0,0 +1,14 @@
+<?php
+
+class Cours {
+    private int $id_cours;
+    private int $id_formateur;
+    private int $id_formation;
+    private int $id_contenu_formation;
+    private string $titre;
+    private float $prix;
+    private string $photo;
+    private DateTime $created_at;
+    
+
+}
