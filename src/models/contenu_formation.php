9873699e4764823bc94f00ef122cd535a78875d3
Maminirina Edwino <edwinomainirina@gmail.com>
2026-02-23 17:50:44 +0300

    implement: getter setter id formation

--- src/models/contenu_formation.php
+++ src/models/contenu_formation.php
@@ -5,4 +5,17 @@ class ContenuFormation {
     private int $id_formation;
     private string $sous_formation;
     private DateTime $created_at;
+
+    public function getIdContenuFormation(): int {
+        return $this->id_contenu_formation;
+    }
+
+    public function getIdFormation(): int {
+        return $this->id_formation;
+    }
+
+    public function setIdFormation(int $idFormation){
+        $this->id_formation = $idFormation;
+    }
+    
 }
