3b3d796b04b0ba74d2a32083ddd75e1538830411
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-02-25 10:49:28 +0300

    implement: resultat quiz model

---
+++ src/models/resultatQuiz.php
@@ -0,0 +1,39 @@
+<?php
+
+class ResultatQuiz {
+    private int $id;
+    private int $utilisateurId;
+    private int $quizId;
+    private int $score;
+    private DateTime $date;
+
+    public function getId(): int {
+        return $this->id;
+    }
+    public function getUtilisateurId(): int {
+        return $this->utilisateurId;
+    }
+    public function setUtilisateurId(int $utilisateurId) {
+        $this->utilisateurId = $utilisateurId;
+    }
+    
+    public function getQuizId(): int {
+        return $this->quizId;
+    }
+
+    public function setQuizId(int $quizId) {
+        $this->quizId = $quizId;
+    }
+    public function getScore(): int {
+        return $this->score;
+    }
+    public function setScore(int $score) {
+        $this->score = $score;
+    }
+    public function getDate(): DateTime {
+        return $this->date;
+    }
+    public function setDate(DateTime $date) {
+        $this->date = $date;
+    }
+}
