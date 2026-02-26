726fc3b4150e61719d119e45851f9483a690c732
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-02-26 19:36:04 +0300

    implement: cours model constructor

--- src/models/cours.php
+++ src/models/cours.php
@@ -9,6 +9,25 @@ class Cours {
     private float $prix;
     private string $photo;
     private DateTime $created_at;
-    
 
+    public function __construct(
+        int $id_formateur,
+        int $id_formation,
+        int $id_contenu_formation,
+        string $titre,
+        float $prix,
+        string $photo
+    )
+    {
+        $this->id_contenu_formation = $id_contenu_formation;
+        $this->id_formateur = $id_formateur;
+        $this->id_formation = $id_formation;
+        $this->titre = $titre;
+        $this->prix = $prix;
+        $this->photo = $photo;
+    }
+
+    
+    
+    
 }
