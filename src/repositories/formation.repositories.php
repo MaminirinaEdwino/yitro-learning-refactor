3ed0c8eff03ffa1d4142e8ae4dac10fefc127c7b
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-02 12:01:06 +0300

    implement: insert function for formation model

--- src/repositories/formation.repositories.php
+++ src/repositories/formation.repositories.php
@@ -3,11 +3,13 @@
 require_once "./src/config/database.php";
 require_once "./src/models/formation.php";
 
-class FormationRepositories{
+class FormationRepositories
+{
     private Database $database;
 
-    private function PushArray($stmt, $result) {
-         while ($donne = $stmt->fetch()){
+    private function PushArray($stmt, $result)
+    {
+        while ($donne = $stmt->fetch()) {
             $formateur = new Formation(
                 $donne["nom_formation"]
             );
@@ -15,5 +17,12 @@ class FormationRepositories{
             $formateur->setIdFormation($donne["id_formation"]);
             array_push($result, $formateur);
         }
+    }
+
+    public function Insert(Formation $formation) {
+        $query = "INSERT INTO formations (nom_formation) VALUES(:formation)";
+        $conn = $this->database->getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->execute(["nom_formation"=>$formation->getNom_formation()]);
     }
 }
