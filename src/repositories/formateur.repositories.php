0f123c0d4d854be153f781bc198bb41608e42e4b
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-02 11:31:07 +0300

    implement: Insert for formateur repositories

--- src/repositories/formateur.repositories.php
+++ src/repositories/formateur.repositories.php
@@ -39,5 +39,38 @@ class FormateurRepositories{
         }
     }
 
+    public function Insert(Formateur $formateur) {
+        $query = "INSERT INTO formateurs(nom_prenom, email, telephone,ville_pays, linkedin, intitule_metier, experience_formation, detail_experience, cv, categories, autre_domaine, titre_cours, objectif, public_cible, detail_complementaire, formats, format_autres, duree_estimee, type_formation, motivation, valeurs, profil_public, statut) VALUES (:nom_prenom, :email, :telephone, :ville_pays, :linkedin, :intitule_metier, :experience_formation, :detail_experience, :cv, :categories, :autre_domaine, :titre_cours, :objectif, :public, :detail_complementaire, :formats, :format_autres, :duree_estimee, :type_formation, :motivation, :valeurs, :profil_public, :statut)";
+
+
+        $conn = $this->database->getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->execute([
+            "nom_prenom"=>$formateur->getNomPrenom(),
+            "email"=>$formateur->getEmail(),
+            "telephone"=>$formateur->getTelephone(),
+            "linkedin"=>$formateur->getLinkedin(),
+            "ville_pays"=>$formateur->getVillePays(),
+            "intitule_metier"=>$formateur->getIntituleMetier(),
+            "experience_formation"=>$formateur->getExperienceFormation(),
+            "detail_experience"=>$formateur->getDetailExperience(),
+            "cv"=>$formateur->getCv(),
+            "categories"=>$formateur->getCategories(),
+            "autre_domainte"=>$formateur->getAutreDomain(),
+            "titre_cours"=>$formateur->getTitreCours(),
+            "objectif"=>$formateur->getObjectif(),
+            "public_cible"=>$formateur->getPublicCible(),
+            "detailcomplementaire"=>$formateur->getDetailComplementaire(),
+            "formats"=>$formateur->getFormats(),
+            "format_autre"=>$formateur->getFormatAutres(),
+            "duree_estimee"=>$formateur->getDureeEstime(),
+            "type_formation"=>$formateur->getTypeFormation(),
+            "motivation">$formateur->getMotivation(),
+            "valeurs"=>$formateur->getValeur(),
+            "profil_public"=>$formateur->getProfilPublic(),
+            "statut"=>$formateur->getStatut()
+        ]);
+    }
+
 
 }
