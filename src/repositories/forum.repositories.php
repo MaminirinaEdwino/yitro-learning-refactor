da4bc3cbda41f89715cb3b17697d5d6e6e99e4ed
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-02 12:23:15 +0300

    create: forum repositories class

---
+++ src/repositories/forum.repositories.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once "./src/config/database.php";
+require_once "./src/models/forum.php";
+
+
+class ForumRepositories{
+    private Database $database;
+
+    private function PushArray($stmt, $result)
+    {
+        while ($donne = $stmt->fetch()) {
+            $forum = new Forum(
+                $donne["nom_formation"]
+            );
+            $forum->setDateCreation($donne['date_creation']);
+            $forum->setId($donne["id"]);
+            array_push($result, $forum);
+        }
+    }
+
+    
+}
