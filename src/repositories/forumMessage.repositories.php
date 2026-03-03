7daf1a58d507bab70b44570fd4fea3bdc86c3f20
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-03 15:01:07 +0300

    implement: Get function for forum message repsoitories

--- src/repositories/forumMessage.repositories.php
+++ src/repositories/forumMessage.repositories.php
@@ -6,15 +6,51 @@ class ForumMessageRepositories
 {
     private Database $database;
 
+    private function PushArray($stmt, $result)
+    {
+        while ($donne = $stmt->fetch()) {
+            $forum = new ForumMessage(
+                $donne["cours_id"],
+                $donne["utilisateur_id"],
+                $donne["message"],
+                $donne["lu"]
+            );
+            $forum->setDate($donne['date']);
+            $forum->setId($donne["id"]);
+            array_push($result, $forum);
+        }
+    }
+
     public function Insert(ForumMessage $forumMessage)
     {
         $query = "INSERT INTO forum_message(cours_id, utilisateur_id, message) VALUES (:cours_id, :utilisateur_id, :message)";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute([
-            "cours_id"=>$forumMessage->getCoursId(),
-            "utilisateur_id"=>$forumMessage->getUtilisateurId(),
-            "message"=>$forumMessage->getMessage()
+            "cours_id" => $forumMessage->getCoursId(),
+            "utilisateur_id" => $forumMessage->getUtilisateurId(),
+            "message" => $forumMessage->getMessage()
         ]);
     }
+
+    public function GetAll(): array {
+        $query = "SELECT * FROM forum_message";
+        $conn = $this->database->getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $result = [];
+        $this->PushArray($stmt, $result);
+        return $result;
+    }
+
+    public function GetById(int $id) : ForumMessage {
+        $query = "SELECT * FROM forum_message WHERE id=:id";
+        $conn = $this->database->getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->execute(["id"=>$id]);
+        $result = [];
+        $this->PushArray($stmt, $result);
+        return $result[0];
+    }
+    
 }
