c9ffbe1e193d0cd08b8f31e88ba48fc187d828a2
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-03 17:27:53 +0300

    implement: inscription repo Insert

--- src/repositories/inscription.repositories.php
+++ src/repositories/inscription.repositories.php
@@ -1,6 +1,34 @@
 <?php
 
+require_once "./src/config/database.php";
+require_once "./src/models/inscription.php";
+
 class InscriptionRepositories {
     private Database $database;
+    
+    private function PushArray($stmt, $result)
+    {
+        while ($donne = $stmt->fetch()) {
+            $forum = new Inscription(
+                $donne["utilisateur_id"],
+                $donne["cours_id"],
+                $donne["statut_payement"]
+            );
+            $forum->setDateInscription($donne['date_inscription']);
+            $forum->setId($donne["id"]);
+            array_push($result, $forum);
+        }
+    }
+
+    public function Insert(Inscription $inscription) {
+        $query = "INSERT INTO inscription (utilisateur_id, cours_id) VALUES(:utilisateur_id, :cours_id)";
+        $conn = $this->database->getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->execute([
+            "utilisateur_id"=>$inscription->getUtilisateurId(),
+            "cours_id"=>$inscription->getCoursId()
+        ]);
+    }
+
     
 }
