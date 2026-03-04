b28b44ae44aa8fbb04d84ea3b0b0eeb21e89769d
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-04 15:49:59 +0300

    implement: question repositories insert function

---
+++ src/repositories/question.repositories.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once "./src/config/database.php";
+require_once "./src/models/question.php";
+
+class QuestionRepositories {
+    private Database $database;
+
+    private function PushArray($stmt, $result)
+    {
+        while ($donne = $stmt->fetch()) {
+            $var = new Question(
+                $donne["quiz_id"],
+                $donne["texte"],
+                $donne["reponse_correcte"],
+                $donne["reponse_incorrecte_1"],
+                $donne["reponse_incorrecte_2"],
+                $donne["reponse_incorrecte_3"]
+            );
+            $var->setId($donne["id"]);
+            array_push($result, $var);
+        }
+    }
+
+    public function Insert(Question $question) {
+        $query = "INSERT INTO question(quiz_id, texte, reponse_correcte, reponse_incorrecte_1,reponse_incorrecte_2, reponse_incorrecte_3) VALUES(:quiz_id, :texte, :reponse_correcte, :reponse_incorrecte_1, :reponse_incorrecte_2, reponse_incorrecte_3)";
+        $conn = $this->database->getConnection();
+        $stmt = $conn->prepare($query);
+        $stmt->execute([
+            "quiz_id"=>$question->getQuizId(),
+            "texte"=>$question->getTexte(),
+            "reponse_correcte"=>$question->getReponseCorrecte(),
+            "reponse_incorrecte_1"=>$question->getReponseIncorrecte1(),
+            "reponse_incorrecte_2"=>$question->getReponseIncorrecte2(),
+            "reponse_incorrecte_1"=>$question->getReponseIncorrecte3()
+        ]);
+    }
+}
