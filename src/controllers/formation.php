1eb60b8dcb2c606056eac611d2cf35791aeef75a
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-12 15:59:02 +0300

    add: formation catalogue controller

--- src/controllers/formation.php
+++ src/controllers/formation.php
@@ -2,8 +2,15 @@
 
 require_once "./src/router/router.php";
 require_once "./src/templaterender/templateRender.php";
+require_once "./src/repositories/formation.repositories.php";
+require_once "./src/repositories/cours.repositories.php";
+
 
 $formationRouter = new Router();
 $formationRouter->get("/formation/catalogue", function(){
-    TemplateRender::render("/formations/catalogue.php", null);
+    $formationRepo = new FormationRepositories();
+    $formations = $formationRepo->GetAll();
+    $coursRepo = new CoursRepositories();
+    $cours = $coursRepo->GetCoursCatalogue();
+    TemplateRender::render("/formations/catalogue.php", ["formations"=>$formations, "cours"=>$cours]);
 });
