feeda3fd37a5f66e8500ac74c2ebe85e7ed5f906
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-15 21:33:02 +0300

    create: catalogue header for logged_in user

---
+++ src/components/headerapprenant2.php
@@ -0,0 +1,27 @@
+<header>
+    <nav class="main-nav">
+        <div class="container">
+            <div class="logo">
+                <img src="<?= URL_ROOT ?>asset/images/logo.png" alt="SK Yitro E-Learning">
+                <a href="/espace/apprenant" class="logo-text">SK Yitro Learning</a>
+            </div>
+            <ul class="nav-list">
+                <li><a href="/formation/catalogue">Catalogues</a></li>
+                <li><a href="progression_apprenant.php">Ma progression</a></li>
+                <li><a href="mes_cours.php">Mes Cours</a></li>
+
+            </ul>
+            <div class="auth-links">
+                <ul class="nav-list">
+                    <li>
+                        <a href="#" class="btn-primary">
+                            <i class="fas fa-user-circle"></i>
+                            <?php echo htmlspecialchars($_SESSION['user_nom']); ?>
+                        </a>
+                    </li>
+                    <li><a href="/logout" class="btn-primary">Déconnexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
