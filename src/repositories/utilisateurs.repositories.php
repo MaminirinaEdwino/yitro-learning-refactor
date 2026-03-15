6f19e7695a7668ad231e1ef166526132ca456710
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-15 15:44:42 +0300

    fix: push array function

--- src/repositories/utilisateurs.repositories.php
+++ src/repositories/utilisateurs.repositories.php
@@ -3,9 +3,10 @@
 require_once "./src/config/database.php";
 require_once "./src/models/utilisateur.php";
 
-class UtilisateursRepositories {
+class UtilisateursRepositories
+{
     private Database $database;
-    private array $result; 
+    private array $result;
 
     public function __construct()
     {
@@ -18,30 +19,32 @@ class UtilisateursRepositories {
             $var = new Utilisateur(
                 $donne["nom"],
                 $donne["email"],
-                $donne["mot_de passe"],
-                $donne["telephone"],
-                $donne["photo"],
-                $donne["pays"],
-                $donne["langue"],
-                $donne["objectifs"],
-                $donne["autre_langue"],
-                $donne("type_cours"),
-                $donne["niveau_formation"],
-                $donne["niveau_etude"],
-                $donne["acces_internet"],
-                $donne["appareil"],
-                $donne["accessibilite"],
-                $donne["rgpd"],
-                $donne["charte"],
-                $donne["role"]
+                $donne["mot_de_passe"],
+                $donne["telephone"] == null ? "" : $donne["telephone"],
+                $donne["photo"] == null ? "" : $donne["photo"],
+                $donne["pays"]  == null ? "" : $donne["pays"],
+                $donne["langue"]  == null ? "" : $donne["langue"],
+                $donne["objectifs"]  == null ? "" : $donne["objectifs"],
+                $donne["autre_langue"] == null ? "" : $donne["autre_langue"],
+                $donne["type_cours"] == null ? "" : $donne["type_cours"],
+                $donne["niveau_formation"] == null ? "" : $donne["niveau_formation"],
+                $donne["niveau_etude"] == null ? "" : $donne["niveau_etude"],
+                $donne["acces_internet"] == null ? "" : $donne["acces_internet"],
+                $donne["appareil"] == null ? "" : $donne["appareil"],
+                $donne["accessibilite"] == null ? "" : $donne["accessibilite"],
+                $donne["rgpd"] == null ? "" : $donne["rgpd"],
+                $donne["charte"] == null ? "" : $donne["charte"],
+                $donne["role"] == null ? "" : $donne["role"]
             );
             $var->setId($donne["id"]);
-            $var->setCreatedAt($donne["date"]);
+            $var->setCreatedAt(new DateTime($donne["created_at"]));
+            $var->setActif($donne["actif"]);
             array_push($this->result, $var);
         }
     }
 
-    public function Insert(Utilisateur $utilisateur) {
+    public function Insert(Utilisateur $utilisateur)
+    {
         $query = "INSERT INTO utilisateurs(
         nom, 
         email,
@@ -83,28 +86,29 @@ class UtilisateursRepositories {
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute([
-        "nom"=>$utilisateur->getNom(), 
-        "email"=>$utilisateur->getEmail(),
-        "mot_de_passe"=>$utilisateur->getMdp(),
-        "telephone"=>$utilisateur->getTelephone(), 
-        "photo"=>$utilisateur->getPhoto(),
-        "pays"=>$utilisateur->getPays(),
-        "langue"=>$utilisateur->getLangue(),
-        "objectifs"=>$utilisateur->getObjectif(),
-        "autre_langues"=>$utilisateur->getAutreLangue(),
-        "type_cours"=>$utilisateur->getTypeCours(),
-        "niveau_formation"=>$utilisateur->getNiveauFormation(),
-        "niveau_etude"=>$utilisateur->getNiveauEtude(),
-        "acces_internet"=>$utilisateur->getAccesInternet(),
-        "appareil"=>$utilisateur->getAppareil(),
-        "accessibilite"=>$utilisateur->getAccessibilite(),
-        "rgpd"=>$utilisateur->getRgpd(),
-        "charte"=>$utilisateur->getCharte(),
-        "role"=>$utilisateur->getRole() 
+            "nom" => $utilisateur->getNom(),
+            "email" => $utilisateur->getEmail(),
+            "mot_de_passe" => $utilisateur->getMdp(),
+            "telephone" => $utilisateur->getTelephone(),
+            "photo" => $utilisateur->getPhoto(),
+            "pays" => $utilisateur->getPays(),
+            "langue" => $utilisateur->getLangue(),
+            "objectifs" => $utilisateur->getObjectif(),
+            "autre_langues" => $utilisateur->getAutreLangue(),
+            "type_cours" => $utilisateur->getTypeCours(),
+            "niveau_formation" => $utilisateur->getNiveauFormation(),
+            "niveau_etude" => $utilisateur->getNiveauEtude(),
+            "acces_internet" => $utilisateur->getAccesInternet(),
+            "appareil" => $utilisateur->getAppareil(),
+            "accessibilite" => $utilisateur->getAccessibilite(),
+            "rgpd" => $utilisateur->getRgpd(),
+            "charte" => $utilisateur->getCharte(),
+            "role" => $utilisateur->getRole()
         ]);
     }
 
-    public function GetAll(): array {
+    public function GetAll(): array
+    {
         $result = [];
         $query = "SELECT * FROM utilisateurs";
         $conn = $this->database->getConnection();
@@ -114,24 +118,25 @@ class UtilisateursRepositories {
         return $result;
     }
 
-    public function GetForAuth(string $email, string $mdp): array {
-        $query = "SELECT * FROM utilisateurs WHERE email = :email, mot_de_passe=:mdp";
+    public function GetForAuth(string $email): Utilisateur
+    {
+        $query = "SELECT * FROM utilisateurs WHERE email = :email";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute([
-            "email"=>$email,
-            "mdp"=>$mdp
+            "email" => $email
         ]);
         $this->PushArray($stmt, null);
-        return $this->result;
+        return $this->result[0];
     }
 
-    public function GetById(int $id): Quiz {
+    public function GetById(int $id): Quiz
+    {
         $result = [];
         $query = "SELECT * FROM utilisateurs WHERE id=:id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
-        $stmt->execute(["id"=>$id]);
+        $stmt->execute(["id" => $id]);
         $this->PushArray($stmt, $result);
         return $result[0];
     }
@@ -161,25 +166,25 @@ class UtilisateursRepositories {
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute([
-        "nom"=>$utilisateur->getNom(), 
-        "email"=>$utilisateur->getEmail(),
-        "mot_de_passe"=>$utilisateur->getMdp(),
-        "telephone"=>$utilisateur->getTelephone(), 
-        "photo"=>$utilisateur->getPhoto(),
-        "pays"=>$utilisateur->getPays(),
-        "langue"=>$utilisateur->getLangue(),
-        "objectifs"=>$utilisateur->getObjectif(),
-        "autre_langues"=>$utilisateur->getAutreLangue(),
-        "type_cours"=>$utilisateur->getTypeCours(),
-        "niveau_formation"=>$utilisateur->getNiveauFormation(),
-        "niveau_etude"=>$utilisateur->getNiveauEtude(),
-        "acces_internet"=>$utilisateur->getAccesInternet(),
-        "appareil"=>$utilisateur->getAppareil(),
-        "accessibilite"=>$utilisateur->getAccessibilite(),
-        "rgpd"=>$utilisateur->getRgpd(),
-        "charte"=>$utilisateur->getCharte(),
-        "role"=>$utilisateur->getRole(),
-        "id"=>$utilisateur->getId() 
+            "nom" => $utilisateur->getNom(),
+            "email" => $utilisateur->getEmail(),
+            "mot_de_passe" => $utilisateur->getMdp(),
+            "telephone" => $utilisateur->getTelephone(),
+            "photo" => $utilisateur->getPhoto(),
+            "pays" => $utilisateur->getPays(),
+            "langue" => $utilisateur->getLangue(),
+            "objectifs" => $utilisateur->getObjectif(),
+            "autre_langues" => $utilisateur->getAutreLangue(),
+            "type_cours" => $utilisateur->getTypeCours(),
+            "niveau_formation" => $utilisateur->getNiveauFormation(),
+            "niveau_etude" => $utilisateur->getNiveauEtude(),
+            "acces_internet" => $utilisateur->getAccesInternet(),
+            "appareil" => $utilisateur->getAppareil(),
+            "accessibilite" => $utilisateur->getAccessibilite(),
+            "rgpd" => $utilisateur->getRgpd(),
+            "charte" => $utilisateur->getCharte(),
+            "role" => $utilisateur->getRole(),
+            "id" => $utilisateur->getId()
         ]);
     }
 
