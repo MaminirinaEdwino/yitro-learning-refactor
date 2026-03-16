bfbcbd76f64852463e378e7f037004659fa36949
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-16 05:12:57 +0300

    implement: getby user coursid function

--- src/repositories/completions.repositories.php
+++ src/repositories/completions.repositories.php
@@ -3,34 +3,40 @@
 require_once "./src/models/completions.php";
 require_once "./src/config/database.php";
 
-class CompletionsRepositories {
+class CompletionsRepositories
+{
     private Database $database;
-    private function PushResult($stmt, $result){
-        while ($donne = $stmt->fetch()){
+    private array $result;
+
+    private function PushResult($stmt, $result)
+    {
+        $this->result = [];
+        while ($donne = $stmt->fetch()) {
             $completion = new Completions($donne['utilisateur_id'], $donne['module_id'], $donne['cours_id']);
             $completion->setDateCompletion($donne['date_completion']);
             $completion->setId($donne["id"]);
-            array_push($result, $completion);
+            array_push($this->result, $completion);
         }
     }
     public function __construct()
     {
         $this->database = new Database;
-        $this->database->getConnection();
     }
-    public function Insert(Completions $completions) {
+    public function Insert(Completions $completions)
+    {
         $query = "INSERT INTO completions (utilisateur_id, module_id, cours_id) VALUES(:utilisateur_id, :module_id, :cours_id)";
         $conn = $this->database->getConnection();
 
         $stmt = $conn->prepare($query);
         $stmt->execute(array(
-            "utilisateur_id"=>$completions->getUtilisateurId(),
-            "module_id"=>$completions->getModuleId(),
-            "cours_id"=>$completions->getCoursId()
+            "utilisateur_id" => $completions->getUtilisateurId(),
+            "module_id" => $completions->getModuleId(),
+            "cours_id" => $completions->getCoursId()
         ));
     }
 
-    public function GetAll(): array{
+    public function GetAll(): array
+    {
         $result = [];
         $query = "SELECT * FROM completions";
         $conn = $this->database->getConnection();
@@ -40,48 +46,60 @@ class CompletionsRepositories {
         return $result;
     }
 
-    public function GetByUtilisateur(int $utilisateurId): array {
+    public function GetByUtilisateur(int $utilisateurId): array
+    {
         $query = "SELECT * FROM completions WHERE utilisateur_id = :id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute(array(
-            "id"=>$utilisateurId
+            "id" => $utilisateurId
         ));
         $result = [];
         $this->PushResult($stmt, $result);
         return $result;
     }
-    public function GetByModuleId(int $module_id): array {
+    public function GetByModuleId(int $module_id): array
+    {
         $query = "SELECT * FROM completions WHERE module_id = :id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute(array(
-            "id"=>$module_id
+            "id" => $module_id
         ));
         $result = [];
         $this->PushResult($stmt, $result);
         return $result;
     }
-    public function GetByCoursId(int $cours_id): array {
+    public function GetByCoursId(int $cours_id): array
+    {
         $query = "SELECT * FROM completions WHERE cours_id = :id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute(array(
-            "id"=>$cours_id
+            "id" => $cours_id
         ));
         $result = [];
         $this->PushResult($stmt, $result);
         return $result;
     }
-    public function GetById(int $id): array {
+    public function GetById(int $id): array
+    {
         $query = "SELECT * FROM completions WHERE id = :id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute(array(
-            "id"=>$id
+            "id" => $id
         ));
         $result = [];
         $this->PushResult($stmt, $result);
         return $result;
+    }
+
+    public function GetByCoursUserId(int $cours_id, $utilisateur_id): array
+    {
+        $stmt = $this->database->getConnection()->prepare("SELECT module_id FROM completions WHERE utilisateur_id = ? AND cours_id = ?");
+        $stmt->execute([$utilisateur_id, $cours_id]);
+        $completed_modules = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        return $completed_modules;
     }
 }
