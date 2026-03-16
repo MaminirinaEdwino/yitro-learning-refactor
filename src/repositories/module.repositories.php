6dc0775fbeef6330e1d55a926983dfb8fddce112
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-16 05:12:19 +0300

    implement: getby coursid function

--- src/repositories/module.repositories.php
+++ src/repositories/module.repositories.php
@@ -3,11 +3,14 @@
 require_once "./src/config/database.php";
 require_once "./src/models/module.php";
 
-class ModuleRepositories {
+class ModuleRepositories
+{
     private Database $database;
+    private array $result;
 
     private function PushArray($stmt, $result)
     {
+        $this->result = [];
         while ($donne = $stmt->fetch()) {
             $var = new Module(
                 $donne["cours_id"],
@@ -15,22 +18,24 @@ class ModuleRepositories {
                 $donne["description"]
             );
             $var->setId($donne["id"]);
-            array_push($result, $var);
+            array_push($this->result, $var);
         }
     }
 
-    public function Insert(Module $module) {
+    public function Insert(Module $module)
+    {
         $query = "INSERT INTO module(cours_id, titre, description) VALUES(:cours_id, :titre, :description)";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute([
-            "cours_id"=>$module->getCoursId(),
-            "titre"=>$module->getTitre(),
-            "description"=>$module->getDescription()
+            "cours_id" => $module->getCoursId(),
+            "titre" => $module->getTitre(),
+            "description" => $module->getDescription()
         ]);
     }
 
-    public function GetAll(): array {
+    public function GetAll(): array
+    {
         $result = [];
         $query = "SELECT * FROM module";
         $conn = $this->database->getConnection();
@@ -40,34 +45,45 @@ class ModuleRepositories {
         return $result;
     }
 
-    public function GetById(int $id): Module {
+    public function GetById(int $id): Module
+    {
         $result = [];
         $query = "SELECT * FROM module WHERE id=:id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
-        $stmt->execute(["id"=>$id]);
+        $stmt->execute(["id" => $id]);
         $this->PushArray($stmt, $result);
         return $result[0];
     }
 
-    public function Update(Module $module) {
+    public function Update(Module $module)
+    {
         $query = "UPDATE module SET cours_id = :cours_id, titre=:titre, description =:description WHERE id=:id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute([
-            "cours_id"=>$module->getCoursId(),
-            "titre"=>$module->getTitre(),
-            "description"=>$module->getDescription(),
-            "id"=>$module->getId()
+            "cours_id" => $module->getCoursId(),
+            "titre" => $module->getTitre(),
+            "description" => $module->getDescription(),
+            "id" => $module->getId()
         ]);
     }
 
-    public function Delete(Module $module) {
+    public function Delete(Module $module)
+    {
         $query = "DELETE FROM module WHERE id = :id";
         $conn = $this->database->getConnection();
         $stmt = $conn->prepare($query);
         $stmt->execute([
             "id" => $module->getId()
         ]);
+    }
+
+    public function GetByCoursId(int $cours_id): array
+    {
+        $stmt = $this->database->getConnection()->prepare("SELECT * FROM modules WHERE cours_id = ?");
+        $stmt->execute([$cours_id]);
+        $this->PushArray($stmt, null);
+        return $this->result;
     }
 }
