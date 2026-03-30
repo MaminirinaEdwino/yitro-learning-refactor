c8c713ea48ec8df2a0856a2ebcab09500772e1fe
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-30 08:26:05 +0300

    update: auth checker

--- src/utils/authChecker.php
+++ src/utils/authChecker.php
@@ -1,8 +1,17 @@
 <?php
 
-function AuthChecker(string $userType): bool{
+function AuthChecker(string $userType): bool
+{
     if ($userType == "admin") {
         if (isset($_SESSION["user_id"]) && $_SESSION['role'] == "admin") {
+            return true;
+        }
+    } else if ($userType == "apprenant") {
+        if (isset($_SESSION["user_id"]) && $_SESSION['role'] == "apprenant") {
+            return true;
+        }
+    } else if ($userType == "formateur") {
+        if (isset($_SESSION["formateur_id"]) && $_SESSION['role'] == "formateur") {
             return true;
         }
     }
